987a60103a4c18bc79c0b8fffc5d74d007769459
Ben Rose <ben.rose@logitbox.com>
2022-10-26 09:51:10 +0100

    WebApi Core Port

---
+++ Redbridge.WebApiCore/Filters/UserNotAuthorizedExceptionFilter.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+using Redbridge.Exceptions;
+
+namespace Redbridge.WebApiCore.Filters
+{
+    public class UserNotAuthorizedExceptionFilter : ExceptionFilterAttribute
+    {
+        readonly ILogger _logger;
+
+        public UserNotAuthorizedExceptionFilter(ILogger<UserNotAuthorizedExceptionFilter> logger)
+        {
+            _logger = logger ?? throw new System.ArgumentNullException(nameof(logger));
+        }
+
+        public override void OnException(ExceptionContext actionExecutedContext)
+        {
+            _logger.LogInformation("Checking exception for user not authorized exception filtering....");
+
+            if (actionExecutedContext.Exception is UserNotAuthorizedException exception)
+            {
+                actionExecutedContext.ExceptionHandled = true;
+                actionExecutedContext.Result = new JsonResult(null) { StatusCode = 403 };
+            }
+        }
+    }
+}
